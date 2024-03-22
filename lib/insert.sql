3d5b9d4dd12bc9f2e65db74594a39794e2f5165b
ChaseTeague <teaguechase@yahoo.com>
2024-03-22 15:52:06 -0400

    copmlete lab

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'Brown', 'Nice', True); 
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Rowdy', 4, 'M', 'Black', 'Mean', True);
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Tabitha', 10, 'F', 'Brown', 'Nice', False); 
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Sergeant Brown', 5, 'M', 'Brown', 'Mean', True); 
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Melissa', 1, 'F', 'Black', 'Nice', True);
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Grinch', 7, 'M', 'Black', 'Mean', False); 
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Wendy', 3, 'F', 'Brown', 'Nice', True); 
-INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES (NULL, 4, 'M', 'Brown', 'Nice', True); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'Brown', 'Nice', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Rowdy', 4, 'M', 'Black', 'Mean', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Tabitha', 10, 'F', 'Brown', 'Nice', 0); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Sergeant Brown', 5, 'M', 'Brown', 'Mean', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Melissa', 1, 'F', 'Black', 'Nice', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Grinch', 7, 'M', 'Black', 'Mean', 0); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ('Wendy', 3, 'F', 'Brown', 'Nice', 1); 
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES (NULL, 4, 'M', 'Brown', 'Nice', 1); 
